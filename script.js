11678f243fca1bdf4271a880e230afc57c00ecb8
in1560 <s6904033612034@email.kmutnb.ac.th>
2026-09-17 15:14:40 +0700

    Update ORDER_ENDPOINT URL in script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
    ============================================================ */
 
 var ROOM_SCENT_CONFIG = {
-  ORDER_ENDPOINT: 'https://script.google.com/macros/s/AKfycbwVW8zip_rkjEvYHJFC7NYb_C_okDgGMRXcUA1C5mPQ1UkBy9v6nh_Tf40Pz8608H27/exec',
+  ORDER_ENDPOINT: 'https://script.google.com/macros/s/AKfycbyKoaeVK8vxOwlwSSGxvgmG4iWRdNHhztOL1ooxriDoJYqkXzi8ZNa1WwSgwYZsqootqg/exec',
   ORDERS_CSV_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSMC1bM6Qa6q0veQGoDOKW5AtoyJqP_fNjllbe-bIkq9rGCD1KZ6kopU9fqd9N4dy4hU9N6-km4gMua/pub?gid=0&single=true&output=csv'
 };
 
